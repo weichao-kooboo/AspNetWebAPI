256484ae3057149227dfd48ef681697ccdf566c4
weichao-kooboo <1264221994@qq.com>
2021-05-16 23:38:03 +0800

    提交更改

--- ASPNETRoute/Global.asax.cs
+++ ASPNETRoute/Global.asax.cs
@@ -19,7 +19,11 @@ namespace ASPNETRoute
             // 在应用程序启动时运行的代码
             //RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configuration.Services.Add(typeof(ModelBinderProvider), new MyTypeConverterModelBinderProvider());
+            //GlobalConfiguration.Configuration.Services.Add(typeof(ModelBinderProvider), new MyTypeConverterModelBinderProvider());
+            RouteValueDictionary defaults = new RouteValueDictionary();
+            defaults.Add("name", "*");
+            defaults.Add("id", "*");
+            RouteTable.Routes.MapPageRoute("default", "employees/{name}/{id}", "~/Pages/RoutePages.aspx", false, defaults);
         }
     }
 }
