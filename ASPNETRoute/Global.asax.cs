11dc3a4140cd16c55e91f3116206f1d0d2075756
weic01 <weic01@mingyuanyun.com>
2021-06-07 00:26:20 +0800

    默认路由

--- ASPNETRoute/Global.asax.cs
+++ ASPNETRoute/Global.asax.cs
@@ -19,7 +19,29 @@ namespace ASPNETRoute
             // 在应用程序启动时运行的代码
             //RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configuration.Services.Add(typeof(ModelBinderProvider), new MyTypeConverterModelBinderProvider());
+            //GlobalConfiguration.Configuration.Services.Add(typeof(ModelBinderProvider), new MyTypeConverterModelBinderProvider());
+
+
+            //1.默认路由规则
+            //RouteValueDictionary defaults = new RouteValueDictionary();
+            //defaults.Add("name", "*");
+            //defaults.Add("id", "*");
+            //RouteTable.Routes.MapPageRoute("default", "employees/{name}/{id}", "~/Pages/RoutePages.aspx", false, defaults);
+
+            //2.
+            RouteValueDictionary defaults = new RouteValueDictionary {
+                {"areacode","010" },
+                {"days",2 }
+            };
+            RouteValueDictionary constaints = new RouteValueDictionary {
+                {"areacode",@"0\d{2,3}" },
+                {"days",@"[1-3]" }
+            };
+            RouteValueDictionary dataTokens = new RouteValueDictionary {
+                {"defaultCity","BeiJing" },
+                {"defaultDays",2 }
+            };
+            RouteTable.Routes.MapPageRoute("default", "{areacode}/{days}", "~/Pages/Weather.aspx", false, defaults, constaints, dataTokens);
         }
     }
 }
